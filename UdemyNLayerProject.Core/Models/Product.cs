64249f078b50db802714aa044c9858d46618d227
Lawrence Peters <lawland2020@outlook.com>
2021-05-08 14:24:03 +0100

    Lawrence: Product vs Category Model(Entity) added

---
+++ UdemyNLayerProject.Core/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyNLayerProject.Core.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool isDeleted { get; set; }
+        public string InnerBaccode { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
