c72760aa3466340823efa8936c82a6ca7cbf3898
Lawrence Peters <lawland2020@outlook.com>
2021-05-08 15:32:37 +0100

    Lawrence: Product vs Category Repostories and Services added

---
+++ UdemyNLayerProject.Core/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyNLayerProject.Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity:class
+    {
+        Task<TEntity> GetByIdAsync(int Id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+
+        //Category.SingleOrDefaultAsync(x=>x.name='saint')
+        Task<TEntity>SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entity);
+        TEntity Update(TEntity entity);
+    }
+}
