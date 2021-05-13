56d04533cdf194a391a806d778707fe06fa7cdc1
Lawrence Peters <lawland2020@outlook.com>
2021-05-13 20:28:46 +0100

    Added Update, Delete Function to Category Controller. GetAll, GetById function to Product Controller

--- UdemyNLayerProject.Api/Controllers/CategoriesController.cs
+++ UdemyNLayerProject.Api/Controllers/CategoriesController.cs
@@ -13,12 +13,12 @@ namespace UdemyNLayerProject.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoryController : ControllerBase
+    public class CategoriesController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
         private readonly IMapper _mapper;
 
-        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
             _mapper = mapper;
@@ -38,11 +38,36 @@ namespace UdemyNLayerProject.Api.Controllers
             return Ok(_mapper.Map<CategoryDto>(category));
         }
 
+        [HttpGet("{id}/products")]
+        public async Task<IActionResult> GetWithProductById(int id)
+        {
+            var category = await _categoryService.GetWithProductByIdAsync(id);
+            return Ok(_mapper.Map<CategoryWithProductDto>(category));
+
+        }
+
         [HttpPost]
         public async Task<IActionResult> Save(CategoryDto categoryDto)
         {
             var newCategory = await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
             return Created(string.Empty, _mapper.Map<CategoryDto>(newCategory));
         }
+
+        [HttpPut]
+        public IActionResult  Update(CategoryDto categoryDto)
+        {
+            var category = _categoryService.Update(_mapper.Map<Category>(categoryDto));
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var category = _categoryService.GetByIdAsync(id).Result;
+            _categoryService.Remove(category);
+            return NoContent();
+
+        }
+
     }
 }
